39a5ceb51f6c15fbd0610f5e7afa58e7753df0b5
arsscriptum <guillaumeplante.qc@gmail.com>
2023-05-08 03:30:56 -0400

    auto

---
+++ test/scripts/Invoke-PostBuild.ps1
@@ -0,0 +1,80 @@
+<#
+#̷𝓍   𝓐𝓡𝓢 𝓢𝓒𝓡𝓘𝓟𝓣𝓤𝓜
+#̷𝓍   🇵​​​​​🇴​​​​​🇼​​​​​🇪​​​​​🇷​​​​​🇸​​​​​🇭​​​​​🇪​​​​​🇱​​​​​🇱​​​​​ 🇸​​​​​🇨​​​​​🇷​​​​​🇮​​​​​🇵​​​​​🇹​​​​​ 🇧​​​​​🇾​​​​​ 🇬​​​​​🇺​​​​​🇮​​​​​🇱​​​​​🇱​​​​​🇦​​​​​🇺​​​​​🇲​​​​​🇪​​​​​🇵​​​​​🇱​​​​​🇦​​​​​🇳​​​​​🇹​​​​​🇪​​​​​.🇶​​​​​🇨​​​​​@🇬​​​​​🇲​​​​​🇦​​​​​🇮​​​​​🇱​​​​​.🇨​​​​​🇴​​​​​🇲​​​​​
+#>
+
+
+function Invoke-IsAdministrator  {  
+    (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
+function Get-ScriptDirectory {
+    Split-Path -Parent $PSCommandPath
+}
+
+      try{
+
+        [string]$ErrorDetails=''
+        [System.Boolean]$ErrorOccured=$False
+
+        ########################################################################################
+        #
+        #  Read Arguments 
+        #
+        ########################################################################################
+        $ExpectedNumberArguments = 1
+        $Script:Arguments = $args.Clone()
+        [System.Collections.ArrayList]$ArgumentList = [System.Collections.ArrayList]::new()
+        0..$ExpectedNumberArguments |  % {
+            $index = $_
+            if([string]::IsNullOrEmpty($Script:Arguments[$index]) -eq $True){
+             Write-Output "missing argument $index"
+            }
+            [void]$ArgumentList.Add("$($Script:Arguments[$index])")
+        }
+
+        [Bool]$DebugMode                = ([string]::IsNullOrEmpty($Script:Arguments[3]) -eq $False)
+
+    
+        [string]$SolutionDirectory      = $ArgumentList.Item(0)
+        [string]$BuiltBinary            = $ArgumentList.Item(1)
+
+        [string]$SolutionDirectory      = (Resolve-Path "$SolutionDirectory").Path
+        [string]$SourcePath             = (Resolve-Path "$SolutionDirectory\src").Path
+        [string]$ScriptsDirectory       = (Resolve-Path "$SolutionDirectory\scripts").Path
+        [string]$BinDirectory           = "$SolutionDirectory\bin"
+        New-Item -Path $BinDirectory -ItemType Directory -Force -ErrorAction Ignore | Out-Null
+
+        Write-Debug "########################################################################################"
+        Write-Debug "                                    DEBUG ARGUMENTS                                     "
+        Write-Debug "`tTargetName          ==> $TargetName"
+        Write-Debug "`tSolutionDirectory   ==> $SolutionDirectory"
+        Write-Debug "########################################################################################"
+
+        ########################################################################################
+        #
+        #  Get Dependencies
+        #
+        ########################################################################################
+
+        [string[]]$deps = . "$ScriptsDirectory\dependencies\GetDependencies.ps1" -Path "$ScriptsDirectory\dependencies"
+        $depscount = $deps.Count
+        $deps | % {
+            $file = $_
+            . "$file"
+            Write-Debug "Include `"$file`""
+        }
+       
+        $Test = Test-Dependencies -Quiet
+        if(! ($Test) ) { throw "dependencies error"} 
+
+        [System.Boolean]$IsAdministrator = Invoke-IsAdministrator 
+
+ 
+        Write-BuildOutTitle "COPY BUILT BINARY"
+        
+        Copy-Item "$BuiltBinary" "$BinDirectory"
+        
+        Write-Output " `"$BuiltBinary`" ==> `"$BinDirectory`"" 
+    }catch{
+        Write-Error "$_"
+    }
